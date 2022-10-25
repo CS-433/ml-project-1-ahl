8c58d99cad81695217b2216a872e2a24b153fbcc
Ajkunas <ajkuna.seipi@epfl.ch>
2022-10-26 00:36:02 +0200

    format

--- implementations.py
+++ implementations.py
@@ -141,7 +141,7 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
 
         # update w by stochastic gradient descent
         w = w - (gamma * grad)
-        
+
         # compute loss
         loss = calculate_mse(random_y, random_tx, w)
 
