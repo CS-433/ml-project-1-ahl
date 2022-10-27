546aa30c899d3c8cb88c1ad3b5e542bb339242ea
Ajkunas <ajkuna.seipi@epfl.ch>
2022-10-27 11:51:16 +0200

    some correction

--- implementations.py
+++ implementations.py
@@ -36,7 +36,7 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
         if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
             break
 
-    return w, losses
+    return w, loss
 
 
 def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
